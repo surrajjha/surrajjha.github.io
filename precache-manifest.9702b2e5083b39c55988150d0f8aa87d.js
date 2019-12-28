117d0752fc306f3af83ee110c056e9025d2f41d0
surrajjha <surrajjha@gmail.com>
2019-12-28 12:28:25 +0530

    portfolio

---
+++ precache-manifest.9702b2e5083b39c55988150d0f8aa87d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b5bdcced172cd367197fd64abe35e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b7179c70eb8c8fb773",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f616b0d155c1c5781b37",
+    "url": "/static/js/2.8093ef63.chunk.js"
+  },
+  {
+    "revision": "b1b7179c70eb8c8fb773",
+    "url": "/static/js/main.57b7d50a.chunk.js"
+  },
+  {
+    "revision": "d26e02afba052cb6ac40",
+    "url": "/static/js/runtime-main.b2213b8f.js"
+  }
+]);
